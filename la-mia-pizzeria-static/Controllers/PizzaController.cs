746f0e55912ee351f6b0d552640cf44982e0f919
Elsti97 <elsti.shehati@hotmail.it>
2023-04-12 14:50:23 +0200

    Update done

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -58,6 +58,43 @@ namespace la_mia_pizzeria_static.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult Update(int id)
+        {
+            var pizza = _context.Pizzas.FirstOrDefault(p => p.Id == id);
+
+            if (pizza is null)
+            {
+                return View("NotFound");
+            }
+
+            return View(pizza);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(pizza);
+            }
+
+            var PizzaUpdate = _context.Pizzas.FirstOrDefault(p => p.Id == pizza.Id);
+
+            if (PizzaUpdate is null)
+            {
+                return View("NotFound");
+            }
+
+            PizzaUpdate.Nome = pizza.Nome;
+            PizzaUpdate.Descrizione = pizza.Descrizione;
+            PizzaUpdate.Prezzo = pizza.Prezzo;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
